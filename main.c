c23728b66fe568280cb4f986f00e49ec70583c50
ufo123456 <64509749+ufo123456@users.noreply.github.com>
2021-07-19 21:27:21 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    float aboat= 32000.0;
+    double abet =2.14e9;
+    printf("%f can written %e\n",aboat,aboat);
+    printf("%f acn witten too %e \n ",aboat,abet);
+
+    return 0;
+}
