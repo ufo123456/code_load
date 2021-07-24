907912c30b562cdd52f149692b1ba3b55d1b787a
ufo123456 <64509749+ufo123456@users.noreply.github.com>
2021-07-24 19:09:43 +0800

    Add files via upload

---
+++ clang/for/first.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{ int conut;
+    int input_num;
+
+    _Bool flag =1;
+
+
+    printf("please your number:");
+
+    scanf("%d",&input_num);
+
+    for (conut = 2; conut < input_num  /2; conut++)
+    {
+        if(input_num %conut==0)
+        {
+
+        }
+    }
+    
+    
+    return 0;
+}
